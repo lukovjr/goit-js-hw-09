348d3474b034a233ddd06cd97f2f2d3aa630c1ae
Oleksy Lykov <lukov_jr@ukr.net>
2023-03-20 22:42:18 +0200

    1/3 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyEl = document.querySelector('body');
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startEl.addEventListener('click', onStartBtnClick);
+let timerId = null;
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  if (timerId) {
+     startEl.disabled = true;
+     stopEl.disabled = false;
+  }
+}
+
+stopEl.addEventListener('click', () => {
+  clearInterval(timerId);
+  startEl.disabled = false;
+  stopEl.disabled = true;
+});
 
